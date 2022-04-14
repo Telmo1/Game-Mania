26f7458f62d1dfb70aa849215fc54f98559962d6
Telmo <yrtelmo00@gmail.com>
2022-04-13 22:19:10 -0300

    Primeiras Implementações javascript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function mostrarMenu() {
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+function pesquisarNewletter () {
+    let pesquisa = document.getElementById("pesquisa").value
+
+    alert (pesquisa)
+    console.log(pesquisa)
+}
